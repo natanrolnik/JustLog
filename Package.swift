2d6a222a8870007b1edfe91f5c48016f18767c90
Natan Rolnik <natanrolnik@gmail.com>
2022-06-09 14:59:13 +0300

    Add Package.swift/resolve

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "JustLog",
+    platforms: [
+        .iOS("13.4"),
+        .tvOS("13.4")
+    ],
+    products: [
+        .library(
+            name: "JustLog",
+            targets: ["JustLog"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/SwiftyBeaver/SwiftyBeaver",
+            .exact("1.9.6")
+	)
+    ],
+    targets: [
+        .target(
+            name: "JustLog",
+            dependencies: ["SwiftyBeaver"],
+                path: "JustLog/",
+            exclude: ["Supporting Files/Info.plist", "Supporting Files/JustLog.h"]),
+    ]
+)
